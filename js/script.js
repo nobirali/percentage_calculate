46c3e2cda9882ee03767fd5f1bba66dc62b2e105
alinobir <gedaali99999@gmail.com>
2019-07-24 00:41:15 +0600

    commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+<script>
+        
+    $(document).ready(function(){
+
+        
+
+    	$("button").click(function(){
+
+    		$("#opv").val((($("#mev").val()*$("#pwd").val())/100));
+
+    		//alert("Percentage Value is "+(($("#mev").val()*$("#pwd").val())/100));
+    		//alert(($("#txt1").val()*$("#txt2").val())/100);
+    	});
+
+        $("#btn1").draggable();
+
+
+    });
+	
+    </script>
---
+++ script.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function(){
+
+        
+
+    	$("button").click(function(){
+
+    		$("#opv").val((($("#mev").val()*$("#pwd").val())/100));
+
+    		//alert("Percentage Value is "+(($("#mev").val()*$("#pwd").val())/100));
+    		//alert(($("#txt1").val()*$("#txt2").val())/100);
+    	});
+
+        
+
+
+    });
+	
